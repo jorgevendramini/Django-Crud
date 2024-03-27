a9381ec6b49c57dbbe460ef952cd5d50bfc6e0cf
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-03-26 22:02:07 -0300

    initial commit

---
+++ todos/views.py
@@ -0,0 +1,21 @@
+from .models import Todo
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
