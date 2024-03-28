4f36371033c62da80f4b01947f70a29ee8446ea4
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-03-28 19:35:59 -0300

    deleteview inserido

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from .models import Todo
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -18,4 +18,9 @@ class TodoCreateView(CreateView):
 class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
     success_url = reverse_lazy("todo_list")
