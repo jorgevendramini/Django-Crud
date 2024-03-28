c88d887c8dc6fbc7a162c8d8c9baca86927e554d
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-03-28 19:40:38 -0300

    concluir tarefa view inserida / CRUD completo

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
 from .models import Todo
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from datetime import date
 
 # Create your views here.
 
@@ -24,3 +26,11 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
