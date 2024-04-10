f2f3c9aaaceed377b037ba2e5225302e9b488cf5
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-04-10 19:39:54 -0300

    feat: added login error message

--- loginPage/views.py
+++ loginPage/views.py
@@ -4,4 +4,18 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, "authenticate\login.html", {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect("todo_list")
+        else:
+            messages.success(request, ("There was an error logging in, Try Again..."))
+            # Return an 'invalid login' error message.
+            return redirect("login")
+    else:
+        pass
+        return render(request, "authenticate\login.html", {})
