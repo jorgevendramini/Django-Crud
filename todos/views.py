7174934a2dba1fe340c58c85bbb6db082b5d1d1a
Jorge Enrique <jorgevendramini@users.noreply.github.com>
2024-04-16 08:56:07 -0300

    Update views.py with a session_key

--- todos/views.py
+++ todos/views.py
@@ -10,12 +10,21 @@ from datetime import date
 class TodoListView(ListView):
     model = Todo
 
+    def get_queryset(self):
+        # Filter todos based on the user's session key
+        session_key = self.request.session.session_key
+        return Todo.objects.filter(session_key=session_key)
+
 
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
+    def form_valid(self, form):
+        # Set the session key for the todo
+        form.instance.session_key = self.request.session.session_key
+        return super().form_valid(form)
 
 class TodoUpdateView(UpdateView):
     model = Todo
