8bdf3395191db97bd09045062c2e1d4d13e2da8d
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-04-16 23:13:21 -0300

    feat: added full functionality to the date format

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,23 +1,34 @@
-console.log("JavaScript code is executing");
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the deadline input field
-    var deadlineInput = document.getElementById("id_deadline");
+    let deadlineInput = document.getElementById("id_deadline");
+    let checkButton = document.getElementById("checkButton");
 
-    // Add an event listener for input events
     deadlineInput.addEventListener("input", function() {
-        // Get the current value of the input field
-        var value = deadlineInput.value;
+        let value = deadlineInput.value;
         value = value.replace(/\D/g, "");
         if (value.length > 2) {
             value = value.substring(0, 2) + "/" + value.substring(2, 4) + "/" + value.substring(4, 8);
         }
-        // Set the modified value back to the input field
         deadlineInput.value = value;
     });
 
+    checkButton.addEventListener("click", function() {
+        const currentDay = () => {
+            let currentDate = new Date();
+            let day = currentDate.getDate();
+            let month = currentDate.getMonth() + 1;
+            let year = currentDate.getFullYear();
+            return new Date(year, month - 1, day); // Note: month - 1 because months are 0-indexed in Date objects
+        }
+
+        let enteredDate = deadlineInput.value;
+        enteredDate = enteredDate.split('/').reverse().join('/'); // Convert date to 'yyyy/mm/dd' format for better parsing
+        if (new Date(enteredDate) < currentDay()) {
+            alert("Please enter a date that is equal to or later than the current date.");
+            deadlineInput.value = "";
+        }
+    });
+
     deadlineInput.addEventListener("keydown", function(event) {
-        // Allow backspace, delete, arrow keys, and numeric keys
         if (
             event.key === "Backspace" ||
             event.key === "Enter" ||
